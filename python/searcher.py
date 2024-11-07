89e2d21c2a005c5e416f7241756d1e6f6671a7d3
Fortox226 <97980099+Fortox226@users.noreply.github.com>
2024-11-07 13:18:48 +0100

    nothimg

--- python/searcher.py
+++ python/searcher.py
@@ -4,7 +4,7 @@ from googleapiclient.discovery import build
 # Ustaw swój klucz API
 API_KEY = 'AIzaSyA8QVQKMP3iCUWcIX5wha6bCPMuA8FS6Rs'
 
-def youtube_search(query, max_results=6):
+def youtube_search(query, max_results=5):
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     request = youtube.search().list(
