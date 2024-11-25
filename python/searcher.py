b28ac574c881d6b2b56a0a0c8d4dfc28ce5c89e1
Bombiasz2721 <twardaktobiasz@gmail.com>
2024-11-25 08:28:57 +0100

    nothing

--- python/searcher.py
+++ python/searcher.py
@@ -2,7 +2,7 @@ import os
 from googleapiclient.discovery import build
 
 # Ustaw swój klucz API
-API_KEY = ''
+API_KEY = 'AIzaSyA8QVQKMP3iCUWcIX5wha6bCPMuA8FS6Rs'
 
 def youtube_search(query, max_results=5):
     youtube = build('youtube', 'v3', developerKey=API_KEY)
